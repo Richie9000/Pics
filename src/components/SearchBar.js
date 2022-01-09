3e897ca43c8b7dd6352dc3caec37de5e262707a1
Ricardo <rr155914@gmail.com>
2022-01-09 09:42:05 -0600

    create components fetch data from API

---
+++ src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+class SearchBar extends React.Component{
+
+    state = { term: ""}
+
+    onFormSubmit = event => {
+        event.preventDefault();
+       
+        this.props.onSubmit(this.state.term) 
+    }
+
+
+    render() {
+        return (
+        <div className="ui segment">
+            <form className="ui form" onSubmit={this.onFormSubmit}>
+                <label htmlFor="">Image search</label>
+                <input className="ui field" type="text" 
+                value={this.state.term} 
+                onChange={e => this.setState({ term: e.target.value })} />
+            </form>
+
+        </div>)
+    }
+}
+
+export default SearchBar
